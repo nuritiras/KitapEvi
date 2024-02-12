ab174bf9c03114ef12acd6dcfd75efaae6b634f5
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2024-02-12 10:47:02 +0300

    Resim dosyası döndüren action metot

---
+++ KitapEvi/Controllers/ResimController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KitapEvi.Controllers
+{
+    public class ResimController : Controller
+    {
+        public FileResult Index()
+        {
+            return File("~/img/meblogo.png", "image/png");
+        }
+    }
+}

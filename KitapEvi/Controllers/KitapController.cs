6de3c9bd4ccd443b6d7663761935025a77f46e70
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2024-02-12 10:05:27 +0300

    Kitap Controller KatmanıProje dosyası ekle.

---
+++ KitapEvi/Controllers/KitapController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KitapEvi.Controllers
+{
+    public class KitapController : Controller
+    {
+        public string Index()
+        {
+            return "Kitap controller sınıfındaki Index metodu çalıştı";
+        }
+        // Diğer action metotlar tanımlanır.
+    }
+}
---
+++ KitapEvi/Models/KategoriModel.cs
@@ -0,0 +1,9 @@
+﻿namespace KitapEvi.Models
+{
+    public class KategoriModel
+    {
+        public int KategoriId { get; set; }
+        public string? KategoriAdi { get; set; }
+        List<KitapModel> kitapListesi=new List<KitapModel>();
+    }
+}

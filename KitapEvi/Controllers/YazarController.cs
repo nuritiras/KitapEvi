6690bf4b14b114107c38340e614d76c621f2c6e6
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2024-02-12 10:28:31 +0300

    Projeye Yazar controller sınıfı eklendi.

---
+++ KitapEvi/Controllers/YazarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KitapEvi.Controllers
+{
+    public class YazarController : Controller
+    {
+        public string Index()
+        {
+            return "Yazar controller sınıfındaki Index metodu çalıştı";
+        }
+    }
+}
